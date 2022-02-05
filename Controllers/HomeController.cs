e25e21bdea6d4f5dbd3a1736c9a677496d34a6e3
aquakolo <aquakolo13@gmail.com>
2022-02-05 19:28:34 +0100

    5_02 Zmiany

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,11 +17,41 @@ namespace ProjektASPNET.Controllers
             return View(model);
         }
 
+        [HttpPost]
+        public ActionResult Index(int productId)
+        {
+            return View(model);
+        }
+
         // zalogowany
         [HttpPost]
         public ActionResult Index(HomeModel model)
         {
             return View(model);
+        }
+
+        public ActionResult CartView()
+        {
+            //var SessionHelper = ProjektASPNET.Helpers.SessionHelper.GetInstance(); 
+            var DB = ProjektASPNET.Helpers.DBHelper.GetInstance();
+            var cart = new OrdersModel();// DB.GetCart(SessionHelper.GetUserID());
+            return View(cart);
+        }
+
+        public ActionResult DeleteFromCartView(int productID)
+        { 
+            var DB = ProjektASPNET.Helpers.DBHelper.GetInstance();
+            var product = new ProductModel();//DB.GetProduct(productID);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFromCartView(ProductModel model)
+        {
+            //var SessionHelper = ProjektASPNET.Helpers.SessionHelper.GetInstance();
+            // usuwanie z bazy danych
+
+            return RedirectToAction("CartView");
         }
     }
 }
